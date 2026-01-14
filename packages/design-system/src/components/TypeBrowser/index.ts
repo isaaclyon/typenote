12931855d8ce3f5a1f8c30a0ae87dfdff6a60da4
Isaac Lyon <isaaclyon@gmail.com>
2026-01-13 23:56:06 -0600

    feat(design-system): export column pinning utilities from TypeBrowser

--- packages/design-system/src/components/TypeBrowser/index.ts
+++ packages/design-system/src/components/TypeBrowser/index.ts
@@ -1,2 +1,4 @@
 export * from './types.js';
 export { TypeBrowser } from './TypeBrowser.js';
+export { ColumnPinMenu } from './ColumnPinMenu.js';
+export { getColumnPinningStyles, buildInitialPinningState } from './pinningStyles.js';
